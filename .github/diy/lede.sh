e311b6a6cef4e3227105ddedb77aeac87dcac118
moruiris <53965900+moruiris@users.noreply.github.com>
2021-12-19 21:45:30 +0800

    Create lede.sh

---
+++ .github/diy/lede.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+##############################################################################################################################################
+##############################################################################################################################################
+#################################################tools编译工具###############################################################################
+#svn co https://github.com/coolsnowwolf/lede/trunk/tools ./tools
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings ./luci-default-settings
+#svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools ./tools
+##############################################################################################################################################
+#################################################弃用##########################################################################################
+########################################luci-app-netdata-实时监控#############################################
+#原作者
+#二次修改 lede不支持弃用
+#git clone https://github.com/sirpdboy/luci-app-netdata
+########################################luci-app-netspeedtest- 网络速度测试#############################################
+#原作者
+#svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
+#rm -rf ./luci-app-netspeedtest/po/zh_Hans
+##############################################################################################################################################
+#################################################软件##########################################################################################
+########################################luci-app-smartdns#############################################
+#原作者
+#git clone https://github.com/pymumu/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns
+#二次修改
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+########################################luci-app-adguardhome##########################################
+#原作者
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome
+#二次修改
+#git clone https://github.com/kongfl888/luci-app-adguardhome
+#最新修改
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
+########################################luci-app-passwall#############################################
+#原作者
+git clone https://github.com/xiaorouji/openwrt-passwall
+mv -n openwrt-passwall/luci-app-passwall ./
+rm -rf ./openwrt-passwall/dns2socks
+rm -rf ./openwrt-passwall/ipt2socks
+rm -rf ./openwrt-passwall/microsocks
+rm -rf ./openwrt-passwall/pdnsd-alt
+########################################luci-app-ssr-plus#############################################
+#原作者
+git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus
+mv -n openwrt-ssr-plus/luci-app-ssr-plus ./
+########################################luci-app-chinadns-ng#############################################
+#原作者
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng ./luci-app-chinadns-ng
+#passwall内置了chinadns-ng，就不用这个重复了
+#git clone https://github.com/pexcn/openwrt-chinadns-ng
+########################################luci-app-oaf-应用过滤#############################################
+#原作者
+#svn co https://github.com/destan19/OpenAppFilter/trunk ./openwrt-oaf
+#mv -n openwrt-oaf/luci-app-oaf ./luci-app-oaf
+############################################################################################################################################################
+#################################################主题########################################################################################################
+#原作者
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config
+
+####################################################################################
+
+
+rm -rf ./*/.git & rm -f ./*/.gitattributes
+rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
+rm -rf ./*/*/.svn & rm -rf ./*/*/.github & rm -rf ./*/*/.gitignore
+exit 0
