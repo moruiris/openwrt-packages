76f1fb6e9cdb6e28ffa385045cf76b1231b21df4
moruiris <53965900+moruiris@users.noreply.github.com>
2021-12-19 22:49:03 +0800

    Update openwrt.sh

--- .github/diy/openwrt.sh
+++ .github/diy/openwrt.sh
@@ -15,10 +15,10 @@ rm -rf $1
 ########################################luci-app-adguardhome##########################################
 #原作者
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome
+#弃用，版本固定 更新不了
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
 #二次修改
-#git clone https://github.com/kongfl888/luci-app-adguardhome
-#最新修改
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome
 ########################################luci-app-chinadns-ng#############################################
 #原作者
 git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng ./luci-app-chinadns-ng
