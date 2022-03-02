6a60594cdea20359c6244f7f207bd961792eb77c
moruiris <53965900+moruiris@users.noreply.github.com>
2022-03-02 15:31:39 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -27,7 +27,7 @@ rm -rf $1
 #git clone https://github.com/pymumu/openwrt-smartdns
 #二次修改
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns ./packages/
+svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns ././openwrt-smartdns
 ########################################luci-app-adguardhome##########################################
 #原作者
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome
@@ -39,17 +39,15 @@ git clone https://github.com/kongfl888/luci-app-adguardhome
 #原作者
 git clone https://github.com/xiaorouji/openwrt-passwall
 mv -n openwrt-passwall/luci-app-passwall ./
-mv -n openwrt-passwall ./packages/passwall
 ########################################luci-app-ssr-plus#############################################
 #原作者
-git clone https://github.com/fw876/helloworld
-mv -n helloworld/luci-app-ssr-plus ./
-mv -n helloworld ./packages/ssr-plus
+git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus
+mv -n openwrt-ssr-plus/luci-app-ssr-plus ./
 ########################################luci-app-chinadns-ng#############################################
 #原作者
 git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng ./luci-app-chinadns-ng
 #passwall内置了chinadns-ng，就不用这个重复了
-git clone https://github.com/pexcn/openwrt-chinadns-ng ./packages/chinadns-ng
+git clone https://github.com/pexcn/openwrt-chinadns-ng
 ############################################################################################################################################################
 #################################################主题########################################################################################################
 ########################################luci-app-argon#############################################
