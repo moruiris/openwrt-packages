361a429773cff1f318f32888e0d94716f9240386
morui <53965900+moruiris@users.noreply.github.com>
2024-07-02 22:05:11 +0800

    Update packages.sh

--- .github/diy/packages.sh
+++ .github/diy/packages.sh
@@ -11,49 +11,47 @@ rm -rf $1
 #原作者
 #二次修改
 #git clone https://github.com/sirpdboy/luci-app-netdata
-########################################luci-app-oaf-应用过滤#############################################
-#原作者
-#svn co https://github.com/destan19/OpenAppFilter/trunk ./openwrt-oaf
-#mv -n openwrt-oaf/luci-app-oaf ./luci-app-oaf
-########################################luci-app-netspeedtest- 网络速度测试#############################################
-#原作者
-#svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
-#rm -rf ./luci-app-netspeedtest/po/zh_Hans
 ##############################################################################################################################################
 #################################################软件##########################################################################################
 ########################################luci-app-smartdns#############################################
 #原作者
-git clone https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns
 #二次修改
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns ././openwrt-smartdns
+git clone https://github.com/kenzok8/openwrt-packages && mv openwrt-packages/luci-app-smartdns ./ && mv openwrt-packages/smartdns ./openwrt-smartdns && rm -rf openwrt-packages
 ########################################luci-app-adguardhome##########################################
 #原作者
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#弃用，版本固定 更新不了
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
 #二次修改
 git clone https://github.com/kongfl888/luci-app-adguardhome
 ########################################luci-app-passwall#############################################
-#原作者
-git clone https://github.com/xiaorouji/openwrt-passwall
-mv -n openwrt-passwall/luci-app-passwall ./
+git clone https://github.com/xiaorouji/openwrt-passwall-packages ./openwrt-passwall
+#rm -rf ./openwrt-passwall/dns2socks
+#rm -rf ./openwrt-passwall/ipt2socks
+#rm -rf ./openwrt-passwall/microsocks
+#rm -rf ./openwrt-passwall/pdnsd-alt
+#原作者 第1版luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall ./passwall && mv passwall/luci-app-passwall ./ && rm -rf passwall
+#原作者 第2版luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2 ./passwall && mv passwall/luci-app-passwall2 ./ && rm -rf passwall
 ########################################luci-app-ssr-plus#############################################
-#原作者
-git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus
-mv -n openwrt-ssr-plus/luci-app-ssr-plus ./
+##原作者
+git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus && mv openwrt-ssr-plus/luci-app-ssr-plus ./
 ########################################luci-app-chinadns-ng#############################################
 #原作者
+#git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng ./luci-app-chinadns-ng
+#passwall内置了chinadns-ng，就不用这个重复了
+#git clone https://github.com/pexcn/openwrt-chinadns-ng
+#二次修改
 git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
 #passwall内置了chinadns-ng，就不用这个重复了
-git clone https://github.com/pexcn/openwrt-chinadns-ng
+#git clone https://github.com/moruiris/openwrt-chinadns-ng
 ############################################################################################################################################################
 #################################################主题########################################################################################################
 ########################################luci-app-argon#############################################
 #原作者
-#git clone https://github.com/jerrykuku/luci-theme-argon.git
-
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config
 ####################################################################################
 
 
