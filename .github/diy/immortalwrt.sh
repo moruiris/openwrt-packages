b40b31ae21a1828d81591cbad87cfb4de547ae6f
morui <53965900+moruiris@users.noreply.github.com>
2024-07-02 21:56:34 +0800

    Update immortalwrt.sh

--- .github/diy/immortalwrt.sh
+++ .github/diy/immortalwrt.sh
@@ -11,15 +11,17 @@ rm -rf $1
 ########################################luci-app-adguardhome##########################################
 #原作者,弃用
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#弃用，版本固定 更新不了
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
 #二次修改
 git clone https://github.com/kongfl888/luci-app-adguardhome
 ########################################luci-app-chinadns-ng#############################################
 #原作者
-git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
+#git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng ./luci-app-chinadns-ng
 #源码已有下列依赖
 #git clone https://github.com/pexcn/openwrt-chinadns-ng
+#二次修改
+git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
+#源码已有下列依赖
+#git clone https://github.com/moruiris/openwrt-chinadns-ng
 ############################################################################################################################################################
 #################################################主题########################################################################################################
 
