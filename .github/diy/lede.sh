6d32c132b04e9b10a438c7776b72d984403e0ca7
morui <53965900+moruiris@users.noreply.github.com>
2024-07-02 21:49:51 +0800

    Update lede.sh

--- .github/diy/lede.sh
+++ .github/diy/lede.sh
@@ -7,14 +7,6 @@ rm -rf $1
 ##############################################################################################################################################
 ##############################################################################################################################################
 #################################################弃用##########################################################################################
-########################################luci-app-oaf-应用过滤#############################################
-#原作者
-#svn co https://github.com/destan19/OpenAppFilter/trunk ./openwrt-oaf
-#mv -n openwrt-oaf/luci-app-oaf ./luci-app-oaf
-########################################luci-app-netspeedtest- 网络速度测试#############################################
-#原作者
-#svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
-#rm -rf ./luci-app-netspeedtest/po/zh_Hans
 ########################################luci-app-chinadns-ng#############################################
 #原作者
 # git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
@@ -26,48 +18,31 @@ rm -rf $1
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns
 #二次修改
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns ././openwrt-smartdns
+git clone https://github.com/kenzok8/openwrt-packages && mv openwrt-packages/luci-app-smartdns ./ && mv openwrt-packages/smartdns ./openwrt-smartdns && rm -rf openwrt-packages
 ########################################luci-app-adguardhome##########################################
 #原作者
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#弃用，版本固定 更新不了
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
 #二次修改
 git clone https://github.com/kongfl888/luci-app-adguardhome
-########################################luci-app-cloudflarespeedtest##########################################
-#原作者
-svn co https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest/trunk/applications/luci-app-cloudflarespeedtest
-git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest ./openwrt-cloudflarespeedtest
 ########################################luci-app-passwall#############################################
-#原作者 第1版luci-app-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
-#原作者 第2版luci-app-passwall2
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall
-#二次作者保留
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall
-#git clone https://github.com/kenzok8/small ./openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages ./openwrt-passwall
 #rm -rf ./openwrt-passwall/dns2socks
 #rm -rf ./openwrt-passwall/ipt2socks
 #rm -rf ./openwrt-passwall/microsocks
 #rm -rf ./openwrt-passwall/pdnsd-alt
+#原作者 第1版luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
+#原作者 第2版luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2 && mv openwrt-passwall2/luci-app-passwall ./ && rm -rf openwrt-passwall2
 ########################################luci-app-ssr-plus#############################################
 #原作者
-git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus
-mv -n openwrt-ssr-plus/luci-app-ssr-plus ./
-########################################luci-app-openclash#############################################
-#原作者
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+git clone https://github.com/fw876/helloworld ./openwrt-ssr-plus && mv -n openwrt-ssr-plus/luci-app-ssr-plus ./
 ############################################################################################################################################################
 #################################################主题########################################################################################################
 ########################################luci-app-argon#############################################
 #原作者
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-#git clone https://github.com/jerrykuku/luci-app-argon-config
-#二次修改
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argonne-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config
 ####################################################################################
 
 
