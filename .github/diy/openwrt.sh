14b1307e0964e3e24b23c234e6e7f9bec327f0d5
morui <53965900+moruiris@users.noreply.github.com>
2025-04-04 21:04:25 +0800

    Update openwrt.sh

--- .github/diy/openwrt.sh
+++ .github/diy/openwrt.sh
@@ -3,30 +3,25 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-############################################################################################################################################################
-#################################################弃用##########################################################################################
+
 ########################################luci-app-smartdns#############################################
 #原作者 openwrt已有
 #git clone https://github.com/pymumu/luci-app-smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns ./luci-app-smartdns-package
 #二次修改
-git clone https://github.com/kenzok8/openwrt-packages && mv openwrt-packages/luci-app-smartdns ./ && mv openwrt-packages/smartdns ./openwrt-smartdns && rm -rf openwrt-packages
-############################################################################################################################################################
-#################################################软件##########################################################################################
+git clone https://github.com/kenzok8/openwrt-packages && mv openwrt-packages/luci-app-smartdns ./ && mv openwrt-packages/smartdns ./luci-app-smartdns-package && rm -rf openwrt-packages
+#####################################################################################################
+
 ########################################luci-app-adguardhome##########################################
 #原作者
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome
 #二次修改
 git clone https://github.com/kongfl888/luci-app-adguardhome
-########################################luci-app-chinadns-ng#############################################
-#原作者
-#git clone -b luci https://github.com/moruiris/openwrt-chinadns-ng ./luci-app-chinadns-ng
-#git clone https://github.com/pexcn/openwrt-chinadns-ng
-############################################################################################################################################################
-#################################################主题########################################################################################################
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-####################################################################################
+#######################################################################################################
 
+#################################################主题##################################################
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+#######################################################################################################
 
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
