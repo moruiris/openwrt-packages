6571eee1335f9172f0048306acece971cd6000c5
morui <53965900+moruiris@users.noreply.github.com>
2025-04-05 14:02:44 +0800

    Update openwrt.sh

--- .github/diy/openwrt.sh
+++ .github/diy/openwrt.sh
@@ -6,7 +6,7 @@ rm -rf $1
 
 ########################################luci-app-smartdns#############################################
 #原作者 openwrt已有
-mkdir ./packages_luci-app-smartdns
+mkdir ./packages_luci-app-smartdns/
 git clone -b master https://github.com/pymumu/luci-app-smartdns && mv ./luci-app-smartdns ./packages_luci-app-smartdns/
 git clone https://github.com/pymumu/openwrt-smartdns ./smartdns && mv ./smartdns ./packages_luci-app-smartdns/
 #####################################################################################################
