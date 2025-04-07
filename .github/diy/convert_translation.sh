dbf015c69bb4487d1d4e9010162e4f50188899bf
morui <53965900+moruiris@users.noreply.github.com>
2025-04-07 14:33:25 +0800

    Delete .github/diy directory

--- .github/diy/convert_translation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for e in $(ls -d luci-*/po); do
-	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
-		ln -s zh-cn $e/zh_Hans
-	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
-		ln -s zh_Hans $e/zh-cn
-	fi
-done
