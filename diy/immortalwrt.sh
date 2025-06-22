1b185e08e1015d25c11a2883901b48591e31a2e7
morui <53965900+moruiris@users.noreply.github.com>
2025-06-22 15:43:42 +0800

    Update immortalwrt.sh

--- diy/immortalwrt.sh
+++ diy/immortalwrt.sh
@@ -11,9 +11,9 @@ rm -rf $1
 #git clone https://github.com/kongfl888/luci-app-adguardhome
 #三次修改
 git clone https://github.com/kenzok8/openwrt-packages
-mkdir ./packages_luci-app-adguardhome
-mv openwrt-packages/luci-app-adguardhome ./packages_luci-app-adguardhome/
-mv openwrt-packages/adguardhome ./packages_luci-app-adguardhome/
+mv openwrt-packages/luci-app-adguardhome ./
+mkdir luci-app-adguardhome_packages
+mv openwrt-packages/adguardhome luci-app-adguardhome_packages/
 rm -rf openwrt-packages
 #######################################################################################################
 
