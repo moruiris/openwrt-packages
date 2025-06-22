1c266e29f4d5d921f6efcc37f9dd32f902175cc4
morui <53965900+moruiris@users.noreply.github.com>
2025-06-22 22:10:08 +0800

    Update openwrt.sh

--- diy/openwrt.sh
+++ diy/openwrt.sh
@@ -6,8 +6,14 @@ rm -rf $1
 
 ########################################luci-app-smartdns#############################################
 #原作者 openwrt已有
-git clone -b master https://github.com/pymumu/luci-app-smartdns 
-git clone https://github.com/pymumu/openwrt-smartdns luci-app-smartdns_packages
+#git clone -b master https://github.com/pymumu/luci-app-smartdns 
+#git clone https://github.com/pymumu/openwrt-smartdns luci-app-smartdns_packages
+#二次修改
+git clone https://github.com/kenzok8/openwrt-packages
+mv openwrt-packages/luci-app-smartdns ./
+mkdir luci-app-smartdns_packages
+mv openwrt-packages/smartdns ./luci-app-smartdns_packages/
+rm -rf openwrt-packages
 #####################################################################################################
 
 ########################################luci-app-adguardhome##########################################
@@ -19,7 +25,7 @@ git clone https://github.com/pymumu/openwrt-smartdns luci-app-smartdns_packages
 git clone https://github.com/kenzok8/openwrt-packages
 mv openwrt-packages/luci-app-adguardhome ./
 mkdir luci-app-adguardhome_packages
-mv openwrt-packages/adguardhome luci-app-adguardhome_packages
+mv openwrt-packages/adguardhome luci-app-adguardhome_packages/
 rm -rf openwrt-packages
 #######################################################################################################
 
