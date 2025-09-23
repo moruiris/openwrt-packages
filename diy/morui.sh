e45d44f346c147752e62d83931512f5377b02d73
morui <53965900+moruiris@users.noreply.github.com>
2025-09-23 09:50:15 +0800

    Create morui.sh

---
+++ diy/morui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+###########################################################################################################################################################
+
+
+########################################luci-app-smartdns#############################################
+#原作者 openwrt已有
+git clone -b master https://github.com/pymumu/luci-app-smartdns 
+git clone https://github.com/pymumu/openwrt-smartdns luci-app-smartdns_packages
+#####################################################################################################
+
+
+###########################################################################################################################################################
+rm -rf ./*/.git & rm -rf ./*/.gitattributes & rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
+rm -rf ./*/*/.git & rm -rf ./*/*/.gitattributes &rm -rf ./*/*/.svn & rm -rf ./*/*/.github & rm -rf ./*/*/.gitignore
+exit 0
