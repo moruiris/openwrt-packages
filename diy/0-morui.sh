0144829db6c5b8da4ce1c41384bb7c7a2ed50c3c
morui <53965900+moruiris@users.noreply.github.com>
2026-01-23 14:17:29 +0800

    Update 0-morui.sh

--- diy/0-morui.sh
+++ diy/0-morui.sh
@@ -8,20 +8,17 @@ rm -rf $1
 
 ###########################################################################################################################################################
 ########################################luci-app-adguardhome##########################################
-mkdir luci-app-adguardhome
-mkdir luci-app-adguardhome_packages
 #原作者,弃用
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome ./luci-app-adguardhome
 #二次修改,弃用
 #git clone https://github.com/kongfl888/luci-app-adguardhome ./luci-app-adguardhome
 #三次修改,弃用
 git clone https://github.com/kenzok8/openwrt-packages
-mv openwrt-packages/luci-app-adguardhome/* ./luci-app-adguardhome
-mv openwrt-packages/adguardhome/* ./luci-app-adguardhome_packages
+mv openwrt-packages/luci-app-adguardhome ./luci-app-adguardhome
+mv openwrt-packages/adguardhome ./luci-app-adguardhome_packages
 rm -rf openwrt-packages
 #######################################################################################################
 ###########################################################################################################################################################
-
 
 
 
