27dba7d33f2b44fec6b825b9c82e443e7b1f89a4
morui <53965900+moruiris@users.noreply.github.com>
2026-01-23 13:52:01 +0800

    Update 2-immortalwrt.sh

--- diy/2-immortalwrt.sh
+++ diy/2-immortalwrt.sh
@@ -4,8 +4,12 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
+
+
+###########################################################################################################################################################
 ########################################luci-app-adguardhome##########################################
 mkdir luci-app-adguardhome
+mkdir luci-app-adguardhome_packages
 #原作者,弃用
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome ./luci-app-adguardhome
 #二次修改,弃用
@@ -13,15 +17,21 @@ mkdir luci-app-adguardhome
 #三次修改,弃用
 git clone https://github.com/kenzok8/openwrt-packages
 mv openwrt-packages/luci-app-adguardhome ./luci-app-adguardhome
-mv openwrt-packages/adguardhome ./luci-app-adguardhome/adguardhome
+mv openwrt-packages/adguardhome ./luci-app-adguardhome_packages
 rm -rf openwrt-packages
 #######################################################################################################
 
-#################################################主题########################################################################################################
-########################################luci-app-argon#############################################
-#mkdir luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-theme-argon.git ./luci-theme-argon
+########################################luci-app-ssr-plus#############################################
+mkdir luci-app-ssr-plus
+mkdir luci-app-ssr-plus_packages
+#原作者
+git clone https://github.com/fw876/helloworld
+mv helloworld/luci-app-ssr-plus ./luci-app-ssr-plus
+mv helloworld/* ./luci-app-ssr-plus_packages
+rm -rf helloworld
+#######################################################################################################
 ###########################################################################################################################################################
+
 
 
 rm -rf ./*/.git & rm -rf ./*/.gitattributes & rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
