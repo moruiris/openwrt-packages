67a95227eed731b0aa09258904262dc91d0dd095
morui <53965900+moruiris@users.noreply.github.com>
2026-04-13 13:21:53 +0800

    Update 1-openwrt.sh

--- diy/1-openwrt.sh
+++ diy/1-openwrt.sh
@@ -4,9 +4,35 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 ###########################################################################################################################################################
-git clone -b morui https://github.com/moruiris/openwrt-packages ./morui
-mv ./morui/app_adguardhome ./app_adguardhome
-mv ./morui/app_smartdns ./app_smartdns
+
+#######################################################################################################
+###########luci-app-adguardhome
+mkdir app_adguardhome
+#原作者,弃用
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
+#二次修改,弃用
+#git clone https://github.com/kongfl888/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
+#三次修改
+git clone https://github.com/kenzok8/openwrt-packages
+mv openwrt-packages/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
+mv openwrt-packages/adguardhome ./app_adguardhome/adguardhome
+rm -rf openwrt-packages
+#######################################################################################################
+
+#######################################################################################################
+###########luci-app-smartdns
+mkdir app_smartdns
+#原作者 openwrt已有
+#git clone -b master https://github.com/pymumu/luci-app-smartdns ./app_smartdns/luci-app-smartdns
+#smartdns依赖路径固定，不能添加，如 include ../../lang/rust/rust-package.mk
+#git clone https://github.com/pymumu/openwrt-smartdns ./app_smartdns/smartdns
+#二次修改
+git clone https://github.com/kenzok8/openwrt-packages
+mv openwrt-packages/luci-app-smartdns ./app_smartdns/luci-app-smartdns
+mv openwrt-packages/smartdns ./app_smartdns/smartdns
+rm -rf openwrt-packages
+#######################################################################################################
+
 #######################################################################################################
 ###########luci-theme-argon
 makdir theme_argon
