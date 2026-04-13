f970aba2da0159320a959414a579460867b675ba
morui <53965900+moruiris@users.noreply.github.com>
2026-04-13 13:11:15 +0800

    Update 2-immortalwrt.sh

--- diy/2-immortalwrt.sh
+++ diy/2-immortalwrt.sh
@@ -3,32 +3,37 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-
-
-
 ###########################################################################################################################################################
-########################################luci-app-adguardhome##########################################
+
+#######################################################################################################
+###########luci-app-adguardhome
+makdir app_adguardhome
 #原作者,弃用
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome ./luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
 #二次修改,弃用
-#git clone https://github.com/kongfl888/luci-app-adguardhome ./luci-app-adguardhome
-#三次修改,弃用
+#git clone https://github.com/kongfl888/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
+#三次修改
 git clone https://github.com/kenzok8/openwrt-packages
-mv openwrt-packages/luci-app-adguardhome ./luci-app-adguardhome
-mv openwrt-packages/adguardhome ./packages-adguardhome
+mv openwrt-packages/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
+mv openwrt-packages/adguardhome ./app_adguardhome/adguardhome
 rm -rf openwrt-packages
 #######################################################################################################
 
-########################################luci-app-ssr-plus#############################################
-#原作者
-#git clone https://github.com/fw876/helloworld
-#mv helloworld/luci-app-ssr-plus ./luci-app-ssr-plus
-#mv helloworld ./packages-ssr-plus
 #######################################################################################################
+###########luci-app-ssr-plus
+makdir app_ssr-plus
+#原作者
+git clone https://github.com/fw876/helloworld
+mv helloworld/luci-app-ssr-plus ./app_ssr-plus/luci-app-ssr-plus
+mv helloworld ./app_ssr-plus/helloworld
+#######################################################################################################
+
+
+
+
+
+
 ###########################################################################################################################################################
-
-
-
 rm -rf ./*/.git & rm -rf ./*/.gitattributes & rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
 rm -rf ./*/*/.git & rm -rf ./*/*/.gitattributes &rm -rf ./*/*/.svn & rm -rf ./*/*/.github & rm -rf ./*/*/.gitignore
 exit 0
