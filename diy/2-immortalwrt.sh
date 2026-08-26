f68490fde0719e8a2027a8f6860827b23b89fcff
morui <53965900+moruiris@users.noreply.github.com>
2026-08-26 18:39:37 +0800

    Update 2-immortalwrt.sh

--- diy/2-immortalwrt.sh
+++ diy/2-immortalwrt.sh
@@ -7,16 +7,16 @@ rm -rf $1
 
 #######################################################################################################
 ###########luci-app-adguardhome
-mkdir app_adguardhome
+#mkdir app_adguardhome
 #原作者,弃用
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
 #二次修改,弃用
 #git clone https://github.com/kongfl888/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
 #三次修改
-git clone https://github.com/kenzok8/openwrt-packages
-mv openwrt-packages/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
-mv openwrt-packages/adguardhome ./app_adguardhome/adguardhome
-rm -rf openwrt-packages
+#git clone https://github.com/kenzok8/openwrt-packages
+#mv openwrt-packages/luci-app-adguardhome ./app_adguardhome/luci-app-adguardhome
+#mv openwrt-packages/adguardhome ./app_adguardhome/adguardhome
+#rm -rf openwrt-packages
 #######################################################################################################
 
 #######################################################################################################
